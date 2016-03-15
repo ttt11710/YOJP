6fdbf0eb28713c64546e771ab2442940cc985f72
tt11710 <273269075@qq.com>
2016-03-15 20:33:11 +0800

    首页

--- YOJP/FreeViewController.swift
+++ YOJP/FreeViewController.swift
@@ -8,122 +8,147 @@
 
 import UIKit
 
-class FreeViewController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout {
+class FreeViewController: UIViewController,UICollectionViewDelegateFlowLayout, UITableViewDelegate,UITableViewDataSource {
     
     
-    var collectionView : UICollectionView!
+    
+    var tableView : UITableView!
+    
+    var showChoose : Bool = false
+    var showSubChoose : Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = UIColor.whiteColor()
-        self.creatCollectionView()
         
+        self.creatTableView()
         // Do any additional setup after loading the view.
     }
     
-    
-    func creatCollectionView() {
-        let flowLayout = UICollectionViewFlowLayout()
-        flowLayout.scrollDirection = .Vertical
-        // flowLayout.headerReferenceSize = CGSizeMake(screenWidth, 37)
-        flowLayout.sectionInset = UIEdgeInsetsMake(2, 0, 0, 0)
-        flowLayout.minimumInteritemSpacing = 2
-        flowLayout.minimumInteritemSpacing = 2
+    func creatTableView() {
         
-        self.view.backgroundColor = UIColor.whiteColor()
-        
-        self.collectionView = UICollectionView(frame: CGRectMake(0, 0, screenWidth, screenHeight-navBarH-titleH), collectionViewLayout: flowLayout)
-        self.collectionView.dataSource = self
-        self.collectionView.delegate = self
-        self.collectionView.backgroundColor = yojpTableViewColor
-        
-        self.view.addSubview(self.collectionView)
-        
-        
-        self.collectionView.registerNib(UINib(nibName: "SearchCollectionReusableView", bundle: nil), forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "SearchCollectionReusableViewId")
-        
-    }
-    
-    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 8
-    }
-    
-    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-        return 1
-    }
-    
-    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        
-        
-            self.collectionView.registerNib(UINib(nibName: "myCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "myCollectionViewCellId")
-            
-            let cellIdentifier : String = "myCollectionViewCellId"
-            let cell : myCollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier(cellIdentifier, forIndexPath: indexPath) as! myCollectionViewCell
-            
-            cell.backgroundColor = UIColor.whiteColor()
-            cell.backImageView.image = UIImage(named: String(format: "image%d", indexPath.row))
-            cell.desLabel.text = "在这个冬天的早晨，静静的看看柏林老宅里的暖心咖啡馆"
-            cell.desLabel.textColor = yojpText
-            cell.desLabel.font = font15
-            cell.dateLabel.text = "1小时前"
-            cell.dateLabel.textColor = yojpLightText
-            cell.dateLabel.font = font14
-            cell.layoutIfNeeded()
-            return cell
-    
-    }
-    
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-        return CGSizeMake(screenWidth/2-2, (screenWidth-10)/8*3+100)
-        
-    }
-    
-    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
-        
-        if indexPath.row == 4 {
-            self.navigationController?.pushViewController(NearbyViewController(), animated: true)
-        }
-            
-        else if indexPath.row != 0 {
-            let cell : myCollectionViewCell = collectionView.cellForItemAtIndexPath(indexPath) as! myCollectionViewCell
-            let string = cell.labelLabel.text
-            
-            let freeDetailViewController : FreeDetailViewController = FreeDetailViewController()
-            
-            if string == "免费券" {
-                freeDetailViewController.ticketType = "免费券"
-            }
-            else if string == "打折券" {
-                freeDetailViewController.ticketType = "九折券"
-            }
-            else if string == "抵扣券" {
-                freeDetailViewController.ticketType = "100元抵扣券"
-            }
-            else if string == "福袋" {
-                freeDetailViewController.ticketType = "福袋"
-            }
-            
-            self.navigationController?.pushViewController(freeDetailViewController, animated: true)
-        }
+        self.tableView = UITableView(frame: CGRectMake(0, 0, screenWidth, screenHeight), style: .Plain)
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.tableFooterView = UIView()
+        self.tableView.backgroundColor = yojpTableViewColor
+        self.view.addSubview(self.tableView)
     }
 
-    func scrollViewDidScroll(scrollView: UIScrollView) {
-        if scrollView.contentOffset.y > oldOffsetY {
-            FirstViewController.shareFirstViewController().escapeBtn.hidden = true
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return section == 0 ? 2 : 10
+    }
+    
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view : UIView = UIView(frame: CGRectMake(0,0,screenWidth,25))
+        view.backgroundColor = yojpTableViewColor
+        
+        
+        let callBtn : CallBackButton = CallBackButton(frame: CGRectMake(screenWidth-80,0,68,30))
+        callBtn.setTitle("打开", forState: .Normal)
+        callBtn.setTitleColor(yojpBlue, forState: .Normal)
+        callBtn.setupBlock()
+        callBtn.callBack = { tag in
+            self.showChoose = !self.showChoose
+            self.tableView.reloadData()
+        }
+        view.addSubview(callBtn)
+        
+        if self.showChoose && !self.showSubChoose {
+            
+            let label : UILabel = UILabel(frame: CGRectMake(0,0,screenWidth,30))
+            label.text = "切换选项"
+            label.backgroundColor = yojpTableViewColor
+            label.textColor = yojpLightText
+            view.addSubview(label)
+            
+            let typeView : UIView = UIView(frame: CGRectMake(0,30,screenWidth,3+3*30))
+            view.addSubview(typeView)
+            
+            let viewWidth = (screenWidth-5)/4
+            
+            for i in 0..<12{
+                let chooseTypeBtn : CallBackButton = CallBackButton(frame: CGRectMake(1*CGFloat(i%4+1)+CGFloat(i%4)*viewWidth,1*CGFloat(i/4+1)+CGFloat(i/4)*30,viewWidth,30))
+                chooseTypeBtn.setTitle(String(format: "商品分类%d", i), forState: .Normal)
+                chooseTypeBtn.setTitleColor(yojpLightText, forState: .Normal)
+                chooseTypeBtn.backgroundColor = UIColor.whiteColor()
+                chooseTypeBtn.setupBlock()
+                chooseTypeBtn.titleLabel?.font = font15
+                chooseTypeBtn.tag = i
+                chooseTypeBtn.callBack = { tag in
+                    print(tag)
+                    self.showSubChoose = true
+                    self.tableView.reloadData()
+                }
+                typeView.addSubview(chooseTypeBtn)
+            }
+        }
+        
+        if self.showChoose && self.showSubChoose {
+            
+            let label : UILabel = UILabel(frame: CGRectMake(0,0,screenWidth,30))
+            label.text = "切换选项"
+            label.backgroundColor = yojpTableViewColor
+            label.textColor = yojpLightText
+            view.addSubview(label)
+            
+            let typeView : UIView = UIView(frame: CGRectMake(0,30,screenWidth,3+2*30))
+            view.addSubview(typeView)
+            
+            let viewWidth = (screenWidth-4)/3
+            
+            for i in 0..<6{
+                    let chooseTypeBtn : CallBackButton = CallBackButton(frame: CGRectMake(1*CGFloat(i%3+1)+CGFloat(i%3)*viewWidth,1*CGFloat(i/3+1)+CGFloat(i/3)*30,viewWidth,30))
+                    chooseTypeBtn.setTitle(String(format: "子分类%d", i), forState: .Normal)
+                    chooseTypeBtn.setTitleColor(yojpLightText, forState: .Normal)
+                    chooseTypeBtn.backgroundColor = UIColor.whiteColor()
+                    chooseTypeBtn.setupBlock()
+                    chooseTypeBtn.titleLabel?.font = font15
+                    chooseTypeBtn.tag = i
+                    chooseTypeBtn.callBack = { tag in
+                        print(tag)
+                        self.showChoose = false
+                        self.showSubChoose = false
+                        self.tableView.reloadData()
+                    }
+                    typeView.addSubview(chooseTypeBtn)
+                }
+            }
+        return view
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        
+        if section == 0 {
+            return 0
         }
         else {
-            FirstViewController.shareFirstViewController().escapeBtn.hidden = false
+            return self.showChoose == false ? 30 : self.showSubChoose == false ? 125 : 95
         }
-        
-        oldOffsetY = scrollView.contentOffset.y
-        
-        if scrollView.contentOffset.y == 0 {
-            FirstViewController.shareFirstViewController().escapeBtn.hidden = false
-        }
-        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+        cell.selectionStyle = UITableViewCellSelectionStyle.None
+        cell.accessoryType = .DisclosureIndicator
+        cell.textLabel?.text = "设置备注"
+        cell.textLabel?.textColor = yojpText
+        cell.textLabel?.font = font15
+        return cell
     }
 
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
